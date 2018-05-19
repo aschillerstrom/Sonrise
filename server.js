31e3d40b7e35b4fe7463710a2a52dedd368953b9
redgemini <wkiddy@gmail.com>
2018-05-19 11:47:13 -0500

    update

--- server.js
+++ server.js
@@ -15,24 +15,34 @@ var db = require("./models");
 // Sets up the Express app to handle data parsing
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 // parse application/json
 app.use(bodyParser.json());
 
 // Sets up handlebars view engine
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
 // Static directory
 app.use(express.static("public"));
 
 // Routes
+var menteeRoutes = require('./routes/mentee_routes');
+var mentorRoutes = require("./routes/mentor_routes");
+var htmlRoutes = require('./routes/html_routes');
 
+menteeRoutes(app);
+mentorRoutes(app);
+htmlRoutes(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
 });
